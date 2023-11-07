8e90713e3cfd3b01dc4865d74bbdedaca49e84df
Mufteev Evgeny <evgenymufteev@gmail.com>
2023-11-07 19:33:03 +0300

    custom hooks useAppDispatch, useAppSelector

---
+++ src/hooks/storeHooks.ts
@@ -0,0 +1,7 @@
+import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux';
+import { AppDispatch, State } from '../types/state';
+
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
