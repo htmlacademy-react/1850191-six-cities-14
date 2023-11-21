e3b98a47ff76a8d41581602115619b07bbe06160
Mufteev Evgeny <evgenymufteev@gmail.com>
2023-11-21 19:50:09 +0300

    удаление пробелов и ненужного селектора

--- src/store/features/offers/selectors.ts
+++ src/store/features/offers/selectors.ts
@@ -3,9 +3,4 @@ import { RootState } from '../../configure-store';
 export const selectCurrentCity = (state: RootState) => state.offers.currentCity;
 export const selectOffers = (state: RootState) => state.offers.offers;
 export const selectCurrentSorting = (state: RootState) => state.offers.currentSorting;
-export const selectUniqueCities = (state: RootState) => {
-  const offers = state.offers.offers;
-  const uniqueCities = Array.from(new Set(offers.map((offer) => offer.city.name)));
-  return uniqueCities;
-};
 export const selectFavoriteOffers = (state: RootState) => state.offers.offers.filter((offer) => offer.isFavorite);
